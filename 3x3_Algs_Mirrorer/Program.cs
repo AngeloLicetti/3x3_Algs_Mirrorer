99d0ede5344bdc7e7f9d1cf90518a20d0ddc16b3
Angelo <angelo.licetti.00@gmail.com>
2019-12-28 11:40:58 -0500

    Revert "3x3 alg mirrorer modified to be 3x3 alg-y2-rotator"
    This reverts commit 2f158de375090b60b8a88e83272172d75bce752c.

--- 3x3_Algs_Mirrorer/Program.cs
+++ 3x3_Algs_Mirrorer/Program.cs
@@ -13,25 +13,20 @@ namespace _3x3_Algs_Mirrorer
             string inAlg;
             while (true)
             {
-                Console.Write("Alg to y2-rotate: ");
+                Console.Write("Alg to mirror: ");
                 inAlg = Console.ReadLine();
-                Console.WriteLine("Alg y2-rotated: {0}\n", mirrorAlg(inAlg));
+                Console.WriteLine("Alg mirrored: {0}\n", mirrorAlg(inAlg));
             }
 
         }
 
         private static string mirrorAlg(string inAlg)
         {
-            if (inAlg == "clr")
-            {
-                Console.Clear();
-                return "Cleared";
-            }
             string[] partes;
             partes = inAlg.Split(' ');
             for(int i=0; i<partes.Length; i++)
             {
-                partes[i] = y2rotationPart(partes[i]);
+                partes[i] = mirrorPart(partes[i]);
             }
             return concatenarPartes(partes);
         }
@@ -47,99 +42,129 @@ namespace _3x3_Algs_Mirrorer
             return salida;
         }
 
-        private static string y2rotationPart(string part)
+        private static string mirrorPart(string part)
         {
             if (part[0].Equals('('))
             {
-                return "(" + y2rotationPart(part.Substring(1));
+                return "(" + mirrorPart(part.Substring(1));
             }
             char lastChar = part[part.Length - 1];
             if (lastChar.Equals(')'))
             {
-                return y2rotationPart(part.Remove(part.Length - 1)) + ")";
+                return mirrorPart(part.Remove(part.Length - 1)) + ")";
             }
             switch (part)
             {
                 //giros normales:
                 case "R":
-                    return "L";
-                case "R'":
                     return "L'";
+                case "R'":
+                    return "L";
                 case "L":
-                    return "R";
-                case "L'":
                     return "R'";
+                case "L'":
+                    return "R";
+                case "U":
+                    return "U'";
+                case "U'":
+                    return "U";
+                case "D":
+                    return "D'";
+                case "D'":
+                    return "D";
                 case "F":
-                    return "B";
+                    return "F'";
                 case "F'":
-                    return "B'";
-                case "B":
                     return "F";
+                case "B":
+                    return "B'";
                 case "B'":
-                    return "f'";
+                    return "B";
 
                 //giros con dos capas:
                 case "r":
-                    return "l";
-                case "r'":
                     return "l'";
+                case "r'":
+                    return "l";
                 case "l":
-                    return "r";
-                case "l'":
                     return "r'";
+                case "l'":
+                    return "r";
+                case "u":
+                    return "u'";
+                case "u'":
+                    return "u";
+                case "d":
+                    return "d'";
+                case "d'":
+                    return "d";
                 case "f":
-                    return "b";
-                case "f'":
-                    return "b'";
-                case "b":
-                    return "f";
-                case "b'":
                     return "f'";
+                case "f'":
+                    return "f";
+                case "b":
+                    return "b'";
+                case "b'":
+                    return "b";
 
                 //giros dobles normales:
                 case "R2":
-                    return "L2";
-                case "R2'":
                     return "L2'";
+                case "R2'":
+                    return "L2";
                 case "L2":
-                    return "R2";
-                case "L2'":
                     return "R2'";
+                case "L2'":
+                    return "R2";
+                case "U2":
+                    return "U2'";
+                case "U2'":
+                    return "U2";
+                case "D2":
+                    return "D2'";
+                case "D2'":
+                    return "D2";
                 case "F2":
-                    return "B2";
-                case "F2'":
-                    return "B2'";
-                case "B2":
-                    return "F2";
-                case "B2'":
                     return "F2'";
+                case "F2'":
+                    return "F2";
+                case "B2":
+                    return "B2'";
+                case "B2'":
+                    return "B2";
 
                 //giros dobles con dos capas:
                 case "r2":
-                    return "l2";
-                case "r2'":
                     return "l2'";
+                case "r2'":
+                    return "l2";
                 case "l2":
-                    return "r2";
-                case "l2'":
                     return "r2'";
+                case "l2'":
+                    return "r2";
+                case "u2":
+                    return "u2'";
+                case "u2'":
+                    return "u2";
+                case "d2":
+                    return "d2'";
+                case "d2'":
+                    return "d2";
                 case "f2":
-                    return "b2";
-                case "f2'":
-                    return "b2'";
-                case "b2":
-                    return "f2";
-                case "b2'":
                     return "f2'";
+                case "f2'":
+                    return "f2";
+                case "b2":
+                    return "b2'";
+                case "b2'":
+                    return "b2";
 
 
                 //giros de capas medias:
                 case "M":
-                    return "M'";
-                case "M'":
                     return "M";
-
-                //default:
+                case "M'":
+                    return "M'";
                 default:
                     return part;
             }
